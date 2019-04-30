023f1d924562f1671393a8eab2107bb3c37a5447
Abhinav Galodha <abhinav9galodha@gmail.com>
2019-04-30 17:41:37 -0500

    Added IsSudoku Test

---
+++ src/Algorithms/Others/Sudoku.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Algorithms.Others
+{
+    public class Sudoku
+    {
+        public static bool IsSudoku(int[,] sudoku)
+        {
+            // Best case Complexity for Sudoku = Quadratic or linear
+            // Best Case complexity for not a sudoku = N
+
+            var isSudoku = true;
+            int rowLength = sudoku.GetLength(0);
+            int columnLength = sudoku.GetLength(1);
+
+            if(rowLength != columnLength)
+            {
+                isSudoku = false;
+                return isSudoku;
+            }
+
+            var expectedSum = (rowLength * (rowLength  + 1))/2;
+
+            for (int rowIndex = 0; rowIndex < rowLength; rowIndex++)
+            {
+                var rowSum = 0;
+                for (int columnIndex = 0; columnIndex < columnLength; columnIndex++)
+                {
+                     rowSum = rowSum + sudoku[rowIndex,columnIndex];
+                }
+                if(rowSum != expectedSum)
+                {
+                    isSudoku = false;
+                    break;
+                }
+            }
+            
+            return isSudoku;
+        }
+    }
+}
---
+++ tests/Algorithms.Test/Other/IsSudokuTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using FluentAssertions;
+
+namespace Algorithms.Test.Other
+{
+    public class IsSudokuTests
+    {
+        [Fact]
+        public void NotASudoku()
+        {
+            var notASudoku = new int[,]
+            {   
+                { 4, 6, 3 },
+                { 5, -2, 1},
+                { 8, 3, 3 }
+            };
+
+            var expectedIsSudoku = false;
+
+            var actualsSudoku = Others.Sudoku.IsSudoku(notASudoku);
+            actualsSudoku.Should().Be(expectedIsSudoku);
+        }
+
+        [Fact]
+        public void IsASudoku()
+        {
+            var aSudoku = new int[,]
+            {   
+                { 1, 2, 3 },
+                { 2, 3, 1},
+                { 3, 1, 2 }
+            };
+
+            var expectedIsSudoku = true;
+
+            var actualsSudoku = Others.Sudoku.IsSudoku(aSudoku);
+            actualsSudoku.Should().Be(expectedIsSudoku);
+        }
+    }
+}
