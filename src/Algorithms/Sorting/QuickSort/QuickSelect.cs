f2edd484bb1a1b9603aa5742ba4cce4b594091d1
Abhinav Galodha <abhinav9galodha@gmail.com>
2019-05-06 17:09:51 -0500

    Quick Select addition and test cases

---
+++ src/Algorithms/Sorting/QuickSort/QuickSelect.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Core;
+
+namespace Algorithms.Sorting
+{
+    public class QuickSelect
+    {
+
+        public static int Find(int[] array, uint indexToFind)
+        {
+            if(array == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            int arrayLength = array.Length;
+
+            if(indexToFind > arrayLength)
+            {
+                throw new ArgumentOutOfRangeException(nameof(indexToFind));
+            }
+
+
+            // Same Strategy as used in the Quick Sort
+            int leftIndex = 0;
+            int rightIndex = arrayLength - 1;
+            
+            //int pivotIndex = (leftIndex + rightIndex) / 2;
+            int pivotIndex = new Random().Next(leftIndex, rightIndex);
+
+            while (leftIndex <  rightIndex)
+            {
+
+            }
+
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ tests/Algorithms.Test/Sorting/QuickSelectTest.cs
@@ -0,0 +1,60 @@
+﻿using Algorithms.Sorting;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Algorithms.Test.Sorting
+{
+    public class QuickSelectTests
+    {
+        [Fact]
+        public void Find3rdLargestInArray()
+        {
+            var inputArray = new int[] {32, 89, 1, 4, 10 };
+
+            var expectedValue = 10;
+
+            var actualValue = QuickSelect.Find(inputArray, 3);
+
+            actualValue.Should().Be(expectedValue);
+        }
+
+        [Fact]
+        public void FindElementAtFirstShouldReturnSmallestElement()
+        {
+            var inputArray = new int[] {32, 89, 1, 4, 10 };
+
+            var expectedValue = 1;
+
+            var actualValue = QuickSelect.Find(inputArray, 1);
+
+            actualValue.Should().Be(expectedValue);
+        }
+
+
+        [Fact]
+        public void FindElementAtLastShouldReturnLargestElement()
+        {
+            var inputArray = new int[] {-32, -89, 1, -4, -10 };
+
+            var expectedValue = 1;
+
+            var actualValue = QuickSelect.Find(inputArray, 5);
+
+            actualValue.Should().Be(expectedValue);
+        }
+
+        [Fact]
+        public void FindElementOutOfRangeShouldThrowException()
+        {
+            var inputArray = new int[] {32, 89, 1, 4, 10 };
+
+            Action action = () => QuickSelect.Find(inputArray , 9);
+            action.Should().Throw<ArgumentOutOfRangeException>();
+        }
+
+        
+    }
+}
