26068424892fa0fa2b42fad7dc80c3a3e854f721
Abhinav Galodha <abhinav9galodha@gmail.com>
2019-06-20 23:46:33 -0500

    Added Tree Datastructure in code

---
+++ src/Algorithms/Tree/Tree.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Tree
+{
+    public class Tree
+    {
+    }
+
+    public class Node<T>
+    {
+        public IEnumerable<T> AdjacentNodes {get; set;}
+
+        public T Value { get; set;}
+
+    }
+}
