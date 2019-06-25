cacc24b04130eab91b5267ff0dcc1912502768cc
Abhinav Galodha <abhinav9galodha@gmail.com>
2019-06-24 19:43:54 -0500

    InOrder Traversal for Tree Started

--- src/DataStructures/Tree/BinaryTree.cs
+++ src/DataStructures/Tree/BinaryTree.cs
@@ -11,7 +11,19 @@ namespace DataStructures.Tree
     {
         public BinaryTree<T> RootNode {get;set;}
 
-        
+        public BinaryTree(T[] arrayOfValuesInAnyOrder)
+        {
+            // How to create a Binary Search Tree, Provided we have any random array which is not sorted.
+            //
+
+            Array
+
+        }
+
+        public static void Create()
+        {
+
+        }
 
     }
 }
