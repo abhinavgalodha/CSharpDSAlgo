1a46c4974faf89f627e1d3a3d2933448dd88feab
Abhinav Galodha <abhinav9galodha@gmail.com>
2019-10-29 17:41:41 -0500

    Added edge class to represent the Edge abstraction

---
+++ src/DataStructures/Graph/Edge.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Core;
+
+namespace DataStructures.Graph
+{
+    public class Edge<T>
+    {
+        public Edge(T source, T destination)
+        {
+            source.ThrowIfNull(nameof(source));
+            source.ThrowIfNull(nameof(destination));
+
+            Source = source;
+            Destination = destination;
+        }
+
+        public T Source { get;}
+
+        public T Destination { get;}
+
+    }
+}
