954d8bb33ea3dc90d4aad83d1d489f01f254b7bc
Abhinav Galodha <abhinav9galodha@gmail.com>
2021-01-30 18:51:32 -0600

    Added BinarySearch Implementation and positive and negative tests.

---
+++ src/Algorithms/Search/BinarySearch.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Search
+{
+    public static class BinarySearch
+    {
+        public static int Search(int[] arrayOfInts, int elementToSearch)
+        {
+            var searchIndex = -1;
+            // Gaurd
+            if (arrayOfInts == null)
+                return searchIndex;
+
+            var startIndex = 0;
+            var endIndex = arrayOfInts.Length;
+            var middleIndex = 0;
+
+            while (startIndex < endIndex)
+            {
+                middleIndex = endIndex % 2 == 0 ? (endIndex / 2) : (endIndex / 2) + 1;
+                var middleElement = arrayOfInts[middleIndex];
+
+                if (elementToSearch == middleElement)
+                {
+                    searchIndex = middleIndex;
+                    break;
+                }
+                else if (elementToSearch > middleElement)
+                {
+                    startIndex = middleIndex;
+                }
+                else
+                {
+                    endIndex = middleIndex;
+                }
+            }
+
+            return searchIndex;
+        }
+    }
+}
---
+++ tests/Algorithms.Test/Search/BinarySearchTests.cs
@@ -0,0 +1,40 @@
+﻿using Algorithms.Search;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Algorithms.Test.Search
+{
+    public class BinarySearchTests
+    {
+        private static IEnumerable<object[]> PositiveTestData { get; } = new List<object[]>
+        {
+            new object[] { new int[]{ 1,2,3 } , 2, 1},
+            new object[] { new int[]{ 1,2,3, 4 } , 2, 1},
+            new object[] { new int[]{ 1,2,3, 4 } , 4, 3},
+            new object[] { new int[]{ 1 } , 1, 0},
+            new object[] { new int[]{-2,-4, -5 } , -5, 2},
+        };
+
+        private static IEnumerable<object[]> NegativeTestData { get; } = new List<object[]>
+        {
+            new object[] { new int[]{ } , 2, -1},
+            new object[] { null , 2, -1},
+            new object[] { new int[]{ 1,2,3 } , 10, -1},
+            new object[] { new int[]{-2,-4, -5 } , -10, -1},
+        };
+
+        public static IEnumerable<Object[]> TestData = PositiveTestData.Union(NegativeTestData);
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void SearchUsingBinarySearch(int[] inputArrayOfInts, int elementToFind, int expectedIndex)
+        {
+            var searchedElementIndex = BinarySearch.Search(inputArrayOfInts, elementToFind);
+            searchedElementIndex.Should().Be(expectedIndex);
+        }
+    }
+}
